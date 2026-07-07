2c891583dd515f816b932ea98a6cc90beac36711
Nama Anda <rey24ti@mahasiswa.pcr.ac.id>
2026-07-07 15:50:22 +0700

    progress selasa

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,15 +4,29 @@ import { routes as adminRoutes } from './MainRoutes.jsx';
 import KaryawanRoutes from './KaryawanRoutes.jsx';
 import GuestRoutes from './GuestRoutes.jsx';
 
+import Login from '../views/pages/Login.jsx';
+import RegisterPage from '../views/pages/Register.jsx';
+
 const routes = [
   ...adminRoutes,
+  // auth routes
+  { path: '/register', element: <RegisterPage /> },
+
+
   KaryawanRoutes,
   GuestRoutes,
+
+  {
+    path: '*',
+    element: null,
+  },
 ];
 
 const router = createBrowserRouter(routes, { basename: '' });
 
 export default router;
+
+
 
 
 
